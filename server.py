5ded4a5725584c29854015b34935bfa2b9963d61
bdfd <bdfd2005@gmail.com>
2022-02-04 14:57:43 -0500

    ✨ feat(Form): Update Data
    input variable and output on webpage

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
 '''
 Author: BDFD
 Date: 2022-02-03 15:32:30
-LastEditTime: 2022-02-04 12:55:40
+LastEditTime: 2022-02-04 14:49:54
 LastEditors: BDFD
 Description: 
 FilePath: \Heroku_Python_Template\server.py
 '''
-from pickle import TRUE
+# from crypt import methods
+# from pickle import TRUE
+# from unittest import result
+# from uuid import RESERVED_FUTURE
 from flask import Flask, render_template, request, redirect
+from forms import SignUpForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY']= 'bdfd2005'
 
 @app.route('/')
 def index():
@@ -30,6 +35,15 @@ def blog():
     author = 'bdfd', 
     sunny = True, 
     posts=posts)
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+  form = SignUpForm()
+  if form.is_submitted():
+    result = request.form
+    print(result)
+    return render_template('user.html', result=result)
+  return render_template('signup.html', form=form)
 
 if __name__ == '__main__':
   app.run()
